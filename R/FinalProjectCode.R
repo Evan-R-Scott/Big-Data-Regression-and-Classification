313425e4e236e1052fce144bf143d152ccf32de4
Evan Scott <evan.scott@dc.rr.com>
2024-04-29 14:25:23 -0700

    removed unnecessary columns from df

--- R/FinalProjectCode.R
+++ R/FinalProjectCode.R
@@ -1,1 +1,13 @@
-df <- read.csv("Premier_League.csv")
+# load original, unmodified dataset
+orig_df <- read.csv("Premier_League.csv")
+
+# clean the dataset of any unnecessary columns and missing data
+clean_df <- clean_data(orig_df)
+
+clean_data <- function(orig_df) {
+  clean_df <- subset(orig_df, select= -c(home_blocked, away_blocked, home_pass, away_pass,
+                                         home_off, away_off, home_offside, away_offside,
+                                         home_tackles, away_tackles, home_duels, away_duels,
+                                         home_saves, away_saves, home_fouls, away_fouls, links))
+  return (clean_df)
+}
